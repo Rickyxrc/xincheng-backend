b667f9f01fe420531135f66176b83ed17c4f92d4
PerssonBrown <Liuyx0808@outlook.com>
2022-07-20 09:33:59 +0800

    Add feat: delete problem

---
+++ backend_api/problems/delete.js
@@ -0,0 +1,31 @@
+let permission = require("../../common/permission");
+let denied = require("../../common/denied");
+let error = require("../../common/error");
+let success = require("../../common/success");
+let badrequest = require("../../common/badrequest");
+let db = require("../../database/conn");
+
+module.exports = (req, res) => {
+    permission(req.query.session, 1)
+    .then((level) => {
+        if (req.query.pid) {
+            db.query("DELETE FROM problems WHERE pid=" + db.escape(req.query.pid) + ';', (err, data) => {
+                if (err) {
+                    console.log(err);
+                    error(res);
+                } else {
+                    success(res);
+                }
+            });
+        } else {
+            badrequest(res);
+        }
+    })
+    .catch((err) => {
+        if (err) {
+            err(res);
+        } else {
+            denied(res);
+        }
+    })
+}
