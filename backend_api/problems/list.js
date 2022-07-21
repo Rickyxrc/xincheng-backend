0a8f8e0b8b57d566526060f8b6c8ddeabfcb3620
Persson Brown <83103475+PerssonBrown@users.noreply.github.com>
2022-07-21 17:51:02 +0800

    Fix bug: fix the bug that the title cannot be paged (#5)
    Fix bug: fix the bug that the title cannot be paged (#5)

--- backend_api/problems/list.js
+++ backend_api/problems/list.js
@@ -5,15 +5,20 @@ var error = require("../../common/error");
 var success = require("../../common/success");
 
 module.exports = (req, res) => {
-	if (req.query.data == undefined) req.query.data = "";
 	if (req.query.limit == undefined || req.query.limit > 20) req.query.limit = 20;
 	if (req.query.page == undefined) req.query.page = 1;
 	permission(req.query["session"], 1)
-		.then((level) => {
-			sql = "SELECT pid,title,difficulty,active FROM problems WHERE title LIKE '%" + req.query.data + "%'" +
-			"OR pid LIKE '%" + req.query.data + "%'" +
-			" LIMIT " + db.escape(req.query.limit) + ";";
-
+		.then(() => {
+			var skipNum = (Number(req.query["page"]) - 1) * Number(req.query["limit"]);
+			
+			if (req.query["data"] == undefined) {
+				sql = "SELECT pid,title,difficulty,active FROM problems LIMIT " + db.escape(skipNum) + ',' + Number(req.query["limit"]) + ";";
+			} else {
+				sql = "SELECT pid,title,difficulty,active FROM problems WHERE title LIKE '%" + req.query.data + "%'" +
+				"OR pid LIKE '%" + req.query.data + "%'" +
+				" LIMIT " + db.escape(skipNum) + ',' + Number(req.query["limit"]) + ";";
+			}
+			 
 			db.query(sql, (err, data) => {
 				if (err) {
 					console.log(err);
@@ -27,25 +32,33 @@ module.exports = (req, res) => {
 				console.log(err);
 				return error(res);
 			}
-			permission(req.query["session"], 0).then((level) => {
-				sql = "SELECT pid,title,difficulty,active FROM problems WHERE active=1 AND (title LIKE '%" + req.query.data + "%'" +
-				"OR pid LIKE '%" + req.query.data + "%')" +
-				" LIMIT " + db.escape(req.query.limit) + ";";
+			permission(req.query["session"], 0)
+				.then(() => {
+					var skipNum = (Number(req.query["page"]) - 1) * Number(req.query["limit"]);
 
-				db.query(sql
-					, (err, data) => {
-						if (err) {
-							console.log(err);
-							return error(res);
-						}
-						return success(res, data);
-					});
-			}).catch((err) => {
-				if (err) {
-					console.log(err);
-					return error(res);
-				}
-				return denied(res);
-			})
+					if (req.query["data"] == undefined) {
+						sql = "SELECT pid,title,difficulty,active FROM problems WHERE active=1"+
+						" LIMIT " + db.escape(skipNum) + ',' + Number(req.query["limit"]) + ";";
+					} else {
+						sql = "SELECT pid,title,difficulty,active FROM problems WHERE title LIKE '%" + req.query.data + "%'" +
+						"OR pid LIKE '%" + req.query.data + "%'" +
+						" LIMIT " + db.escape(skipNum) + ',' + Number(req.query["limit"]) + ";";
+					}
+
+					db.query(sql
+						, (err, data) => {
+							if (err) {
+								console.log(err);
+								return error(res);
+							}
+							return success(res, data);
+						});
+				}).catch((err) => {
+					if (err) {
+						console.log(err);
+						return error(res);
+					}
+					return denied(res);
+				})
 		});
 };
