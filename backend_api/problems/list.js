00b9e5d17a248f2ab991bac3df35f2779bdaaf13
Rickyxrc <rickyxrc@outlook.com>
2022-07-22 16:47:44 +0800

    fix bugs:cannot search problem title

--- backend_api/problems/list.js
+++ backend_api/problems/list.js
@@ -5,60 +5,76 @@ var error = require("../../common/error");
 var success = require("../../common/success");
 
 module.exports = (req, res) => {
-	if (req.query.limit == undefined || req.query.limit > 20) req.query.limit = 20;
-	if (req.query.page == undefined) req.query.page = 1;
-	permission(req.query["session"], 1)
-		.then(() => {
-			var skipNum = (Number(req.query["page"]) - 1) * Number(req.query["limit"]);
-			
-			if (req.query["data"] == undefined) {
-				sql = "SELECT pid,title,difficulty,active FROM problems LIMIT " + db.escape(skipNum) + ',' + Number(req.query["limit"]) + ";";
-			} else {
-				sql = "SELECT pid,title,difficulty,active FROM problems WHERE title LIKE '%" + req.query.data + "%'" +
-				"OR pid LIKE '%" + req.query.data + "%'" +
-				" LIMIT " + db.escape(skipNum) + ',' + Number(req.query["limit"]) + ";";
-			}
-			 
-			db.query(sql, (err, data) => {
-				if (err) {
-					console.log(err);
-					return error(res, err.message);
-				}
-				return success(res, data);
-			});
-		})
-		.catch((err) => {
-			if (err) {
-				console.log(err);
-				return error(res);
-			}
-			permission(req.query["session"], 0)
-				.then(() => {
-					var skipNum = (Number(req.query["page"]) - 1) * Number(req.query["limit"]);
+  if (req.query.limit == undefined || req.query.limit > 20)
+    req.query.limit = 20;
+  if (req.query.page == undefined) req.query.page = 1;
+  permission(req.query["session"], 1)
+    .then(() => {
+      var skipNum =
+        (Number(req.query["page"]) - 1) * Number(req.query["limit"]);
 
-					if (req.query["data"] == undefined) {
-						sql = "SELECT pid,title,difficulty,active FROM problems WHERE active=1"+
-						" LIMIT " + db.escape(skipNum) + ',' + Number(req.query["limit"]) + ";";
-					} else {
-						sql = "SELECT pid,title,difficulty,active FROM problems WHERE title LIKE '%" + req.query.data + "%'" +
-						"OR pid LIKE '%" + req.query.data + "%'" +
-						" LIMIT " + db.escape(skipNum) + ',' + Number(req.query["limit"]) + ";";
-					}
+      if (db.query.data == undefined) db.query.data = "";
 
-					db.query(sql
-						, (err, data) => {
-							if (err) {
-								console.log(err);
-								return error(res);
-							}
-							return success(res, data);
-						});
-				}).catch((err) => {
-					if (err) {
-						console.log(err);
-						return error(res);
-					}
-					return denied(res);
-				})
-		});
+      sql =
+        "SELECT pid,title,difficulty,active FROM problems WHERE title LIKE '%" +
+        req.query.data +
+        "%'" +
+        " OR pid LIKE '%" +
+        req.query.data +
+        "%'" +
+        "ORDER BY pid LIMIT " +
+        Number(skipNum) +
+        "," +
+        Number(req.query["limit"]) +
+        ";";
+
+      db.query(sql, (err, data) => {
+        if (err) {
+          console.log(err);
+          return error(res, err.message);
+        }
+        return success(res, data);
+      });
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+        return error(res);
+      }
+      permission(req.query["session"], 0)
+        .then(() => {
+          var skipNum =
+            (Number(req.query["page"]) - 1) * Number(req.query["limit"]);
+
+          if (req.query["data"] == undefined) req.query.data = "";
+
+          sql =
+            "SELECT pid,title,difficulty,active FROM problems WHERE title LIKE '%" +
+            req.query.data +
+            "%'" +
+            " OR pid LIKE '%" +
+            req.query.data +
+            "%'" +
+            " AND active=0 ORDER BY pid LIMIT " +
+            Number(skipNum) +
+            "," +
+            Number(req.query["limit"]) +
+            ";";
+          console.log(sql);
+          db.query(sql, (err, data) => {
+            if (err) {
+              console.log(err);
+              return error(res);
+            }
+            return success(res, data);
+          });
+        })
+        .catch((err) => {
+          if (err) {
+            console.log(err);
+            return error(res);
+          }
+          return denied(res);
+        });
+    });
 };
